6f70d90404499542b3256fc07b0d437a540756ea
Elmir <duck-3@mail.ru>
2023-01-10 22:06:13 +0300

    Initial Commit

---
+++ TestMail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMail
+//
+//  Created by Эльмир Юсупов on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
